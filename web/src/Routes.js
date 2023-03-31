9da3d75cd42d0027f5cf424b8b0b0fc84f123c35
Jason McInerney <jason.j.mcn@gmail.com>
2023-03-31 16:25:15 -0700

    Rename shell script Add Routes, a layout and refactor the pages

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -1,19 +1,13 @@
-// In this file, all Page components from 'src/pages` are auto-imported. Nested
-// directories are supported, and should be uppercase. Each subdirectory will be
-// prepended onto the component name.
-//
-// Examples:
-//
-// 'src/pages/HomePage/HomePage.js'         -> HomePage
-// 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
-
-import { Router, Route } from '@redwoodjs/router'
+import { Router, Route, Set } from '@redwoodjs/router'
+import GeneralLayout from 'src/layouts/GeneralLayout'
 
 const Routes = () => {
   return (
     <Router>
-      <Route path="/about" page={AboutPage} name="about" />
-      <Route path="/" page={HomePage} name="home" />
+      <Set wrap={GeneralLayout}>
+        <Route path="/about" page={AboutPage} name="about" />
+        <Route path="/" page={HomePage} name="home" />
+      </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
   )
--- web/src/layouts/GeneralLayout/GeneralLayout.js
+++ web/src/layouts/GeneralLayout/GeneralLayout.js
@@ -1,7 +1,21 @@
 import { Link, routes } from '@redwoodjs/router'
 
 const GeneralLayout = ({ children }) => {
-  return <>{children}</>
+  return (
+    <>
+    <header>
+      <h1>TicketMeister</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to={routes.about()}>About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>{children}</main>
+  </>
+  )
 }
 
 export default GeneralLayout
