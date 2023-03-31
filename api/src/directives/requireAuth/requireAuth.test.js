706ddf316d76f679ba3622a6f425097aaa9d89e0
Jason McInerney <jason.j.mcn@gmail.com>
2023-03-31 00:15:24 -0700

    Remove hot shots from the code base

--- api/src/directives/requireAuth/requireAuth.test.js
+++ api/src/directives/requireAuth/requireAuth.test.js
@@ -1,11 +1,5 @@
 import { mockRedwoodDirective, getDirectiveName } from '@redwoodjs/testing/api'
 import requireAuth from './requireAuth'
-const HotShots = require('hot-shots');
-const client = new HotShots({
-  host: 'ec2-35-90-12-71.us-west-2.compute.amazonaws.com',
-  port: 8125,
-  prefix: 'myapp.test.',
-});
 
 describe('requireAuth directive', () => {
   it('declares the directive sdl as schema, with the correct name', () => {
